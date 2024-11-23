dfbcc557f75f299c0925ccbce4ee125cd7710de6
Damiencastro <75629680+Damiencastro@users.noreply.github.com>
2024-11-22 21:50:32 -0500

    Update filter.service.ts

--- client/src/app/shared/services/filter.service.ts
+++ client/src/app/shared/services/filter.service.ts
@@ -3,22 +3,7 @@ import { Injectable } from "@angular/core";
 @Injectable({ providedIn: 'root' })
 export class FilteringService {
 
-  compareTimestampDates(itemTimestamp: Timestamp, filterValue: Date | Timestamp | string): boolean {
-    const itemDate = itemTimestamp.toDate();
-    const filterDate = filterValue instanceof Date 
-      ? filterValue 
-      : filterValue instanceof Timestamp 
-        ? filterValue.toDate()
-        : new Date(filterValue);
 
-    return this.isSameDay(itemDate, filterDate);
-  }
-
-  private isSameDay(date1: Date, date2: Date): boolean {
-    return date1.getFullYear() === date2.getFullYear() &&
-           date1.getMonth() === date2.getMonth() &&
-           date1.getDate() === date2.getDate();
-  }
 
   filter<T extends Record<string, any>>(items: T[], filters: any, filterProperties: string[]): T[] {
     return items.filter((item: T) => {
