b8eef09add655f5716d5e01431a215ca244230e4
damiencastro <email@damiencastro.dev>
2024-12-06 19:24:31 -0500

    Fixed approveEntry to block only posted/approved entries from being re-approved

--- client/src/app/portalModule/journalEntry/journal-entries.facade.ts
+++ client/src/app/portalModule/journalEntry/journal-entries.facade.ts
@@ -114,7 +114,7 @@ export class JournalEntryFacade {
    */
   approveEntry(entry: JournalEntry) {
     this.loadingSubject.next(true);
-    if(entry.status !== JournalEntryStatus.PENDING) { throw new Error("Entry is not pending approval") }
+    if(entry.status === JournalEntryStatus.APPROVED || entry.status === JournalEntryStatus.POSTED) { throw new Error("Entry is not pending approval") }
 
     firstValueFrom(this.userProfileState.userProfile$).then(user => {
       console.log('Current user ID:', user.id);
